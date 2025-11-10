55df29fd88dab952ed4ef9bb40c51522b87abfe8
Suren Poghosyan <surenpoghosyan@Surens-MacBook-Pro-3.local>
2025-11-10 22:57:06 +0400

    Added empty project to repo

---
+++ MultipleStockListApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultipleStockListApp
+//
+//  Created by Suren Poghosyan on 10.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultipleStockListApp/MultipleStockListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleStockListAppApp.swift
+//  MultipleStockListApp
+//
+//  Created by Suren Poghosyan on 10.11.25.
+//
+
+import SwiftUI
+
+@main
+struct MultipleStockListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
