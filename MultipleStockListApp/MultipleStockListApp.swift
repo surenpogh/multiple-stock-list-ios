348d0dd3b56401d3e3bdb9ce193c3a8a56f4f183
Suren Poghosyan <surenpoghosyan@Surens-MacBook-Pro-3.local>
2025-11-11 17:57:31 +0400

    Added deep link handling

--- MultipleStockListApp/MultipleStockListApp.swift
+++ MultipleStockListApp/MultipleStockListApp.swift
@@ -10,10 +10,11 @@ import SwiftUI
 @main
 struct MultipleStockListApp: App {
     @StateObject private var stockDataService = StockDataService()
+    @State private var navigationPath = NavigationPath()
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
+            NavigationStack(path: $navigationPath) {
                 FeedView(stockDataService: stockDataService)
                     .navigationDestination(for: NavigationDestination.self) { destination in
                         switch destination {
@@ -24,6 +25,22 @@ struct MultipleStockListApp: App {
                         }
                     }
             }
+            .onOpenURL { url in
+                handleDeepLink(url)
+            }
         }
+    }
+    
+    // MARK: - Deep Link Handling
+    
+    private func handleDeepLink(_ url: URL) {
+        guard url.scheme == "stocks",
+              url.host == "symbol",
+              let symbolId = url.pathComponents.last else {
+            return
+        }
+        
+        // Navigating to the symbol detail screen
+        navigationPath.append(NavigationDestination.symbolDetail(symbolId))
     }
 }
